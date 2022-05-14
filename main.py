1ff9c7d055aeb341a0ab3a61ccfa978cc8b5b07e
Mary <65444001+milsmary@users.noreply.github.com>
2022-05-14 14:19:40 +0300

    connected with DataBase and logins saved in json

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import telegram
 import feedparser
+import psycopg2
 from time import sleep
+import datetime
+import sys
+import json
 
 
 def format_fancy_message(new_item):
@@ -13,14 +17,41 @@ def format_fancy_message(new_item):
     return good_message
 
 
-bot = telegram.Bot(token='5299733983:AAHHkZK3kHq0psPqlovDNDlDxsG4Bn0bDnY')
+def insert_upd_in_registered_chats(upd, conn):
+    now = datetime.datetime.now()
+    cursor = conn.cursor()
+    query = f"INSERT INTO registered_chats VALUES({upd['message']['chat']['id']}, " \
+            f"'{upd['effective_user']['username']}', '{now}') ON CONFLICT DO NOTHING"
+    print(query)
+    cursor.execute(query)
+    conn.commit()
+
+
+def choose_chat_ids(conn):
+    cursor = conn.cursor()
+    query = 'SELECT chat_id FROM registered_chats'
+    cursor.execute(query)
+    list_chat_ids = []
+    for s in cursor.fetchall():
+        list_chat_ids.append(s[0])
+    return list_chat_ids
+
+
+config_file = open('config.json', 'r')
+config_data = config_file.read()
+config = json.loads(config_data)
+conn = psycopg2.connect(**config['connection'])  # распаковываем второй вложенный словарь в json по ключам
+
+bot = telegram.Bot(token=config['bot_token'])
 chat_ids = set()
 arxiv_cs_ds_url = 'http://arxiv.org/rss/cs.DS'
 sent = set()
 
+
 while True:
+    print('new_iteration')
     # new incoming message in the bot
-    updates = bot.get_updates()
+    updates = bot.get_updates(read_latency = 10)
     feed = feedparser.parse(arxiv_cs_ds_url)
 
     new_item = None
@@ -36,8 +67,11 @@ while True:
     # gathering chat ids for incoming messages
     for upd in updates:
         chat_ids.add(upd['message']['chat']['id'])
-    for chat_id in chat_ids:
+        if '/start' in upd['message']['text']:
+            print('start found')
+            insert_upd_in_registered_chats(upd, conn)
+
+    for chat_id in choose_chat_ids(conn):
         bot.send_message(text=format_fancy_message(new_item),
                          chat_id=chat_id, parse_mode=telegram.constants.PARSEMODE_HTML)
     sent.add(new_item['link'])
-    print(chat_ids)
